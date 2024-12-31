67796816051d1eb3f40bbeadf6de1229d320d5e8
Ryan Harris <ryanh96@me.com>
2024-12-30 23:57:21 -0500

    Add initial player/truck controllers

--- code/PlayerController.cs
+++ code/PlayerController.cs
@@ -1,22 +1,105 @@
+using System;
+
 namespace scraprunners;
 
-public sealed class PlayerController : Component
+public class PlayerController : Component
 {
+
+
+	[Property]
+	public GameObject FirstPersonCam;
+	[Property]
+	public Vector3 ThirdPersonCamOffsets;
+
+	[Property]
+	private bool IsFirstPerson = true;
+
+	[Property]
+	public float MovementSpeed;
+
+	public CameraComponent Camera;
+	public Angles EyeAngles;
+	protected override void OnStart()
+	{
+		Camera = Scene.GetAllComponents<CameraComponent>().First();
+	}
 
 	protected override void OnUpdate()
 	{
 		HandleUseInput();
+		HandleViewInput();
 
+		PositionCamera();
+		PositionPlayer();
 	}
 
 	private void HandleUseInput()
 	{
-
-		if ( Input.Down( "use" ) )
+		if ( Input.Pressed( "use" ) )
 		{
-			// Log.Info( SandboxPlayerController.Pressed );
-		}
+			Log.Info( "use" );
+			Gizmo.Draw.Line( FirstPersonCam.WorldPosition, FirstPersonCam.WorldPosition + Camera.WorldRotation.Forward * 50f );
+			var tr = Scene.Trace.Ray( FirstPersonCam.WorldPosition, FirstPersonCam.WorldPosition + Camera.WorldRotation.Forward * 50f ).WithoutTags( "player" ).Run();
+			if ( tr.Hit )
+			{
+				Log.Info( tr.GameObject );
+				var ctrl = tr.GameObject.GetComponents<TruckController>( includeDisabled: true );
+				if ( ctrl.Any() )
+				{
+					var truck = ctrl.First();
+					Log.Info( "TRUCK DETECTED!!!" );
+					truck.Drive(this);
 
+				}
+			}
+		}
 	}
 
+	private void HandleViewInput()
+	{
+		if ( Input.Pressed( "view" ) )
+		{
+			Log.Info( "view" );
+			IsFirstPerson = !IsFirstPerson;
+		}
+	}
+
+	private void PositionPlayer()
+	{
+		var wishPosition = (Input.AnalogMove * EyeAngles.ToRotation()).WithZ( 0f ).Normal;
+		WorldPosition += wishPosition * MovementSpeed;
+		WorldRotation = EyeAngles.WithRoll( 0 ).WithPitch( 0 ).ToRotation();
+	}
+
+	private void PositionCamera()
+	{
+		EyeAngles += Input.AnalogLook;
+		EyeAngles.roll = 0;
+		EyeAngles.pitch = EyeAngles.pitch.Clamp( -89.9f, 89.9f );
+		Camera.WorldRotation = Rotation.From( EyeAngles );
+
+		// FirstPersonPosition.WorldPosition = WorldPosition + FirstPersonCam;
+
+		if ( IsFirstPerson )
+		{
+			Camera.WorldPosition = FirstPersonCam.WorldPosition;
+		}
+		else
+		{
+			var camTrace = Camera.WorldRotation.Backward * ThirdPersonCamOffsets.x;
+			camTrace += Camera.WorldRotation.Left * ThirdPersonCamOffsets.y;
+			camTrace += Camera.WorldRotation.Up * ThirdPersonCamOffsets.z;
+
+			var tr = Scene.Trace.Ray( FirstPersonCam.WorldPosition, WorldPosition + camTrace ).Run();
+			if ( tr.Hit )
+			{
+				Camera.WorldPosition = tr.HitPosition + camTrace.Normal;
+			}
+			else
+			{
+				Camera.WorldPosition = tr.EndPosition;
+
+			}
+		}
+	}
 }
--- code/TruckController.cs
+++ code/TruckController.cs
@@ -2,43 +2,32 @@ using System;
 using Sandbox;
 using static Sandbox.Component;
 
-public sealed class TruckController : Component, IPressable
+namespace scraprunners;
+
+public sealed class TruckController : PlayerController
 {
 
-	public GameObject Driver;
+	public PlayerController Driver;
 
-	public bool Press( IPressable.Event e )
+	public void Drive( PlayerController pc )
 	{
-		Transform t = WorldTransform.Add(Vector3.Forward * 100f, true);
-		Driver = e.Source.GameObject;
-		// Driver.WorldTransform = t;
-		return true;
+		Driver = pc;
+
+		this.Enabled = true;
+		Driver.Enabled = false;
 	}
 
 	protected override void OnUpdate()
 	{
 
-		Log.Info(Driver);
-		if (Driver != null)
-		{
-			Log.Info("Changing camera");
-			// var camera = Scene.GetComponent<CameraComponent>();
-			// camera.WorldTransform = WorldTransform.Add((Vector3.Up * 100f) + (Vector3.Backward * 100f), true);
-			HandleDriverInput();
-			var pc = Driver.GetComponent<PlayerController>();
-			if (pc.IsValid()) pc.Enabled = false;
-		}
+		base.OnUpdate();
 
-	}
-
-	private void HandleDriverInput()
-	{
 		if (Input.Pressed("use"))
 		{
-			var pc = Driver.GetComponent<PlayerController>();
-			if (pc.IsValid()) pc.Enabled = true;
-			Driver = null;
+			Log.Info("TRUCK USE!!");
+			Driver.Enabled = true;
+			this.Enabled = false;
 		}
-
 	}
+
 }
