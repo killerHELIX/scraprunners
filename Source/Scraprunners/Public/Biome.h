0863d60d0697c867f5603e707c100f7b1f486e55
jaketaylornorris <jaketaylornorris@gmail.com>
2025-01-27 19:51:17 -0500

    deleted unnecessary files and started refactoring terrain code

---
+++ Source/Scraprunners/Public/Biome.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Biome.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCRAPRUNNERS_API UBiome : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	UBiome();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome");
+	int Seed = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome");
+	int Width;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome");
+	int Length;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome");
+	float Height;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome");
+	float Scale;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome");
+	float NoiseFrequency;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome");
+	float HeightMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Biome");
+	float ScalarForNoise;
+
+	TArray<FVector2D> VertexPositions;
+};
